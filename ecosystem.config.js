4fdcf4177c106f732d566759a494c963234fd3ff
Shobowale Damilare <oluwadamilareshobowale@gmail.com>
2025-05-21 14:21:10 +0100

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         name: 'newgold-survey',
         script: 'main.js',
         env: {
-          MONGO_URL: 'mongodb://ac-mrflpie-shard-00-00.kv8slwx.mongodb.net:27017,ac-mrflpie-shard-00-01.kv8slwx.mongodb.net:27017,ac-mrflpie-shard-00-02.kv8slwx.mongodb.net:27017/?replicaSet=atlas-vb54yo-shard-0" --ssl --authenticationDatabase admin --username tayeshobajo --password 1Manchester_sm%21',
+          MONGO_URL: 'mongodb://tayeshobajo:1Manchester_sm%21@cluster0-shard-00-00.kv8slwx.mongodb.net:27017,cluster0-shard-00-01.kv8slwx.mongodb.net:27017,cluster0-shard-00-02.kv8slwx.mongodb.net:27017/bioptrics-demo?ssl=true&replicaSet=atlas-kv8slwx-shard-0&authSource=admin&retryWrites=true&w=majority',
           ROOT_URL: 'https://survey.bioptrics.com',
           PORT: 3000,
           NODE_ENV: 'production'
