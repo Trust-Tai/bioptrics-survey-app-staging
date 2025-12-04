5a2daa04bf45c6bda0a3776f142d6ab44f0afe52
Shobowale Damilare <oluwadamilareshobowale@gmail.com>
2025-12-04 22:43:18 +0100

    cleanups

--- imports/core/index.ts
+++
@@ -1,7 +0,0 @@
-// Core module exports
-export * from './auth';
-export * from './database';
-export * from './utils';
-export * from './contexts';
-export * from './components';
-export * from './services';
